05a80746688db5616f7c1c0d18c86e81df4ecbde
Zeeshan <Zeeshan1234802@gmail.com>
2022-08-28 13:49:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
+  const createDigits = () => {
+    let digits = []
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button onClick={()=>updateCalc(i.toString())} key={i}>{i}</button>)
+    }
+    return digits
+  }
+
+  const [result, setResult] = useState("")
+  const [calc, setCalc] = useState("")
+
+  const operators=["*","/",".","+","-"]
+
+  const updateCalc=(value)=>{
+    if((operators.includes(value) && calc==="") || (operators.includes(value) && operators.includes(calc.slice(-1)))){
+      return
+    }
+    setCalc(calc+value)
+
+    if(!operators.includes(value)){
+      setResult(eval(calc+value))
+    }
+  }
+  const calculate=()=>{
+    setCalc(eval(calc).toString())
+  }
+
+  const correction=()=>{
+    if(calc===""){
+      return
+    }
+    setCalc(calc.slice(0,-1))
+  }
+  const reset=()=>{
+    setCalc("")
+    setResult("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+
+        <h1>Calculator</h1>
+        <div className="display">
+          <span>{result? <span>({result}){" "}</span> : ""}</span>
+          <p>{calc || "0"}</p>
+        </div>
+
+        <div className="operators">
+          <button onClick={()=>updateCalc("*")}>*</button>
+          <button  onClick={()=>updateCalc("/")} >/</button>
+          <button  onClick={()=>updateCalc("+")}>+</button>
+          <button  onClick={()=>updateCalc("-")}>-</button>
+          <button onClick={()=>correction()}>correct</button>
+          <button onClick={()=>reset()} >C</button>
+        </div>
+
+        <div className="digits">
+          {createDigits()}
+          <button onClick={()=>updateCalc(0)}>0</button>
+          <button  onClick={()=>updateCalc(".")}>.</button>
+
+          <button onClick={()=>calculate()}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
